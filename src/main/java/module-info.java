148c9f3475a760787ab90c76be9fe3289fe114dd
Eetu Soronen <89025008+soronen@users.noreply.github.com>
2024-09-17 15:38:20 +0300

    add gluonhq/maps openstreetmap library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module org.tuni.stargazer {
-    requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires com.gluonhq.maps;
 
     opens org.tuni.stargazer to javafx.fxml;
     exports org.tuni.stargazer;
--- src/main/java/org/tuni/stargazer/HelloController.java
+++ src/main/java/org/tuni/stargazer/HelloController.java
@@ -1,14 +1,34 @@
 package org.tuni.stargazer;
 
+import com.gluonhq.maps.MapView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private VBox rootPane;
+
+    @FXML
+    private void initialize() {
+        welcomeText.setText("initialize");
+        MapView mv = createMapView();
+        rootPane.getChildren().add(mv);
+
+
+    }
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    private MapView createMapView() {
+        MapView mapView = new MapView();
+        mapView.setPrefSize(400, 400);
+        return mapView;
     }
 }
